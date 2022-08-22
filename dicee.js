78da7daf95586eb8dbe9c7db82c8d56dd529c6e8
Shivang Gangwar <75557079+shivgang@users.noreply.github.com>
2022-08-22 18:37:41 +0530

    Add files via upload
    Deploying Initial Files

---
+++ dicee.js
@@ -0,0 +1,16 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomnumber2=Math.floor(Math.random()*6)+1;
+
+var image1="images/dice"+randomnumber1+".png";
+var image2="images/dice"+randomnumber2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",image1);
+document.querySelectorAll("img")[1].setAttribute("src",image2);
+
+if(randomnumber1>randomnumber2){
+  document.querySelector("h1").textContent="🚩Player1 wins!!";
+}else if(randomnumber1<randomnumber2){
+    document.querySelector("h1").textContent="🚩Player2 wins!!";
+}else{
+  document.querySelector("h1").textContent="Draw";
+}
